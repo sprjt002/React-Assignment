497b70f601345117602418af052bb5cc88537452
jonospras <sprjt002@mymail.unisa.edu.au>
2025-11-03 20:23:37 +1030

    edited appearance of webpage

--- src/App.js
+++ src/App.js
@@ -108,7 +108,7 @@ useEffect(() => {
 
 return (
     <div>
-        <h2>Strudel Demo</h2>
+        <h2 className="header-title">Strudel Demo</h2>
         <main>
 
             <div className="container-fluid">
